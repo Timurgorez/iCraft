a2a8d096b80955b4130e8686a2728d29805a88ef
zhanna19 <funnygirl322@gmail.com>
2020-08-25 14:33:24 +0300

    Product slider

--- src/components/CardArea/ProductGallery.vue
+++ src/components/CardArea/ProductGallery.vue
@@ -5,59 +5,46 @@
     </div>
   <div class="thumb-example d-flex flex-row">
     <!-- swiper1 -->
-
-    <swiper class="swiper gallery-top" ref="swiperTop" :options="swiperOptionTop" >
-      <swiper-slide class="slide-1"></swiper-slide>
-      <swiper-slide class="slide-2"></swiper-slide>
-      <swiper-slide class="slide-3"></swiper-slide>
-      <swiper-slide class="slide-4"></swiper-slide>
-      <swiper-slide class="slide-5"></swiper-slide>
-<!--      <div class="swiper-button-next swiper-button-white" slot="button-next"></div>-->
-<!--      <div class="swiper-button-prev swiper-button-white" slot="button-prev"></div>-->
-    </swiper>
-
+    <div class="gallery-top" v-swiper:swiperTop="swiperOptionTop">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide slide-1"></div>
+        <div class="swiper-slide slide-2"></div>
+        <div class="swiper-slide slide-3"></div>
+        <div class="swiper-slide slide-4"></div>
+        <div class="swiper-slide slide-5"></div>
+      </div>
+    </div>
     <!-- swiper2 Thumbs -->
-    <swiper class="swiper gallery-thumbs" ref="swiperThumbs" :options="swiperOptionThumbs" >
-      <swiper-slide class="slide-1"></swiper-slide>
-      <swiper-slide class="slide-2"></swiper-slide>
-      <swiper-slide class="slide-3"></swiper-slide>
-      <swiper-slide class="slide-4"></swiper-slide>
-      <swiper-slide class="slide-5"></swiper-slide>
-    </swiper>
+    <div class="gallery-thumbs" v-swiper:swiperThumbs="swiperOptionThumbs">
+      <div class="swiper-wrapper" style="top: -60px;">
+        <div class="swiper-slide slide-1"></div>
+        <div class="swiper-slide slide-2"></div>
+        <div class="swiper-slide slide-3"></div>
+        <div class="swiper-slide slide-4"></div>
+        <div class="swiper-slide slide-5"></div>
+      </div>
+    </div>
   </div>
   </div>
 </template>
 
 <script>
-import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
 export default {
   name: "ProductGallery",
-  components: {
-    Swiper,
-    SwiperSlide
-  },
   data() {
     return {
       swiperOptionTop: {
         loop: true,
         loopedSlides: 5, // looped slides should be the same
         spaceBetween: 10,
-        controller: {
-          control: this.swiperThumbs,
-          by: 'slide'
-        },
           navigation: {
-          nextEl: '.thumb-example .swiper-button-next',
-          prevEl: '.thumb-example .swiper-button-prev'
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
         }
       },
       swiperOptionThumbs: {
         direction: 'vertical',
         loop: true,
-        controller: {
-          control: this.swiperTop,
-          by: 'slide'
-        },
         loopedSlides: 5, // looped slides should be the same
         spaceBetween: 10,
         centeredSlides: true,
@@ -67,20 +54,14 @@ export default {
       }
     }
   },
-  computed: {
-    swiperTop() {
-      return this.$refs.swiperTop.$swiper
-    },
-    swiperThumbs() {
-      return this.$refs.swiperThumbs.$swiper
-    }
-  },
   mounted() {
-    this.$nextTick(() => {
-      console.log(this.swiperTop, this.swiperThumbs)
-     this.swiperTop.params.controller.control = this.swiperThumbs
-     this.swiperThumbs.params.controller.control = this.swiperTop
-    })
+    const self = this;
+    this.swiperThumbs.on("slideChange", function() {
+      self.swiperTop.slideTo(this.activeIndex);
+    });
+    this.swiperTop.on("slideChange", function() {
+      self.swiperThumbs.slideTo(this.activeIndex);
+    });
   }
 }
 </script>
@@ -119,7 +100,6 @@ export default {
   }
 }
 
-.swiper {
   .swiper-slide {
     background-size: cover;
     background-position: center;
@@ -141,11 +121,11 @@ export default {
     }
   }
 
-  &.gallery-top {
+  .gallery-top {
     width: 100%;
     height: 100%;
   }
-  &.gallery-thumbs {
+  .gallery-thumbs {
     box-sizing: border-box;
     top: 13px;
     position: absolute;
@@ -155,20 +135,17 @@ export default {
     height: 490px;
     right: 40px;
 
-    &.swiper-wrapper {
-      top: -60px !important;
-    }
   }
 
-  &.gallery-thumbs .swiper-slide {
+  .gallery-thumbs .swiper-slide {
     height: 23%;
     width: 100%;
     opacity: 1;
     border-radius: 6px;
     border: solid 2px #ffffff;
   }
-  &.gallery-thumbs .swiper-slide-active {
+  .gallery-thumbs .swiper-slide-active {
     border: solid 2px $purple_color_btn;
   }
-}
+
 </style>
